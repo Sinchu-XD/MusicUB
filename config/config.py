ffc7391ff7829db82be6ba60e3520f26601f4c0f
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2025-02-07 19:56:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,15 +7,15 @@ Copyright ©️ 2025
 import os
 
 
-API_ID: int = int(os.getenv("API_ID", 0))
+API_ID: int = int(os.getenv("API_ID", 7249983))
 
-API_HASH: str = os.getenv("API_HASH", "")
+API_HASH: str = os.getenv("API_HASH", "be8ea36c220d5e879c91ad9731686642")
 
-SESSION_STRING: str = os.getenv("SESSION_STRING", "")
+SESSION_STRING: str = os.getenv("SESSION_STRING", "BQBuoD8AuTI0qg2FHVUmNjJn_prTBuZPGkRs76iKazfwNytYFXeN4bZJ3N_NAh7eFnic_mRIuTP2mXyFlBFR0XxTIjaXiB26hRtg_qfZ-dWT2UslBACBpn7ifYB4YPnx7wVM7t05HSdp1S0F72PhIrA1UCwYroWkbMdn8Xa3IU1AbqEabViWxHJ09mwRYmiybRgwU-EQgLG-PgsIRK7iqHRrufOuGCgW_sxOWuuI8c6Z_qO3OntrzAtoFkPboIHRS6CLddEI0p6RqY7_zSJjBC5IyjmVS--9jKbhFpb6uVDXJ9a7eaxFLv7Rq9N9_wMNylqAgcTPUufybNWTOxl_ao_xaIDjwgAAAAFklkDwAA")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 0))]
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 7143123520))]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", -4694535872))
 
 PREFIX: str = str(".")
 
