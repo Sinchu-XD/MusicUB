5ffd309f316a0f7bcee97668e1743d497c2b09b8
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2025-02-07 16:05:23 +0530

    Create YtDetails.py

---
+++ Player/Utils/YtDetails.py
@@ -0,0 +1,52 @@
+"""
+Telegram @Itz_Your_4Bhi
+Copyright ©️ 2025
+"""
+
+
+from pytubefix import Search, YouTube as pyYouTube, Playlist
+from urllib.parse import urlparse, parse_qs
+
+
+def searchYt(query, is_videoId=False):
+    query = str(query)
+    if is_videoId:
+        video = pyYouTube(f"https://www.youtube.com/watch?v={query}")
+        title = video.title
+        duration = video.length
+        link = video.watch_url
+        return title, duration, link
+    else:
+        videosResult = Search(query)
+        for Result in videosResult.videos:
+            title = Result.title
+            duration = Result.length
+            link = Result.watch_url
+            return title, duration, link
+    return None, None, None
+
+
+def searchPlaylist(query):
+    query = str(query)
+    playlistResult = Playlist(query)
+    title = playlistResult.title
+    duration = playlistResult.length
+    return title, duration
+
+
+def extract_playlist_id(url):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    playlist_id = query_params.get("list", [None])[0]
+    return playlist_id
+
+
+def extract_video_id(url):
+    parsed_url = urlparse(url)
+    if parsed_url.hostname == "youtu.be":
+        video_id = parsed_url.path[1:]
+    else:
+        query_params = parse_qs(parsed_url.query)
+        video_id = query_params.get("v", [None])[0]
+    return video_id
+  
