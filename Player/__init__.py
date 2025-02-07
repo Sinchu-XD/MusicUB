384de64697efb6ffbfdc81ddcc3b4fe5612b9e0e
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2025-02-07 15:50:17 +0530

    Create __init__.py

---
+++ Player/__init__.py
@@ -0,0 +1,13 @@
+"""
+Telegram @Itz_Your_4Bhi
+Copyright ©️ 2025
+"""
+
+from Player.Core.bot import MusicBot, MusicUser
+from .logging import LOGGER
+from Player.Misc import sudo
+
+sudo()
+
+app = MusicBot
+call = MusicUser
