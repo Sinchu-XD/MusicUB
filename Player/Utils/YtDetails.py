6d208fe1d6ac3357ae30a9d3190748048455df69
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2025-02-09 15:52:27 +0530

    Update YtDetails.py

--- Player/Utils/YtDetails.py
+++ Player/Utils/YtDetails.py
@@ -4,17 +4,18 @@ Copyright ©️ 2025
 """
 
 # Nothing #
-from pytubefix import Search, YouTube as pyYouTube, Playlist
+from youtubesearchpython import VideosSearch, PlaylistsSearch
 from urllib.parse import urlparse, parse_qs
 
 
-def searchYt(query, is_videoId=False):
+def searchYt(query):
     query = str(query)
-    if is_videoId:
-        video = pyYouTube(f"https://www.youtube.com/watch?v={query}", use_po_token=True)
-        title = video.title
-        duration = video.length
-        link = video.watch_url
+    videosResult = VideosSearch(query, limit=1)
+    Result = videosResult.result()
+    if not Result["result"] == []:
+        title = Result["result"][0]["title"]
+        duration = Result["result"][0]["duration"]
+        link = Result["result"][0]["link"]
         return title, duration, link
     else:
         videosResult = Search(query, use_po_token=True)
@@ -28,25 +29,28 @@ def searchYt(query, is_videoId=False):
 
 def searchPlaylist(query):
     query = str(query)
-    playlistResult = Playlist(query, use_po_token=True)
-    title = playlistResult.title
-    duration = playlistResult.length
-    return title, duration
+    playlistResult = PlaylistsSearch(query, limit=1)
+    Result = playlistResult.result()
+    if not Result["result"] == []:
+        title = Result["result"][0]["title"]
+        videoCount = Result["result"][0]["videoCount"]
+        link = Result["result"][0]["link"]
+        return title, videoCount, link
+    return None, None, None
 
 
 def extract_playlist_id(url):
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
-    playlist_id = query_params.get("list", [None])[0]
-    return playlist_id
+    playlist_id = query_params.get('list', [None])[0]
 
 
 def extract_video_id(url):
     parsed_url = urlparse(url)
-    if parsed_url.hostname == "youtu.be":
+    if parsed_url.hostname == 'youtu.be':
         video_id = parsed_url.path[1:]
     else:
         query_params = parse_qs(parsed_url.query)
-        video_id = query_params.get("v", [None])[0]
+        video_id = query_params.get('v', [None])[0]
     return video_id
   
