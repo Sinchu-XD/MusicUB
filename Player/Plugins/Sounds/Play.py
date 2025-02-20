92dceb14015b45911f8569b51c0985e859ee8658
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-20 18:16:44 +0530

    Update Play.py

--- Player/Plugins/Sounds/Play.py
+++ Player/Plugins/Sounds/Play.py
@@ -25,13 +25,31 @@ PREFIX = config.PREFIX
 RPREFIX = config.RPREFIX
 
 
+def cookiefile():
+    cookie_dir = "Player/cookie"
+    cookies_files = [f for f in os.listdir(cookie_dir) if f.endswith(".txt")]
+
+    cookie_file = os.path.join(cookie_dir, cookies_files[0])
+    return cookie_file
+
 async def ytdl(format: str, link: str):
-    stdout, stderr = await bash(
-        f'yt-dlp --geo-bypass -g -f "{format}" {link}'
-    )
-    if stdout:
-        return 1, stdout
-    return 0, stderr
+    ydl_optssx = {
+                "format": "bestaudio/best",
+                "cookiefile": cookiefile(),
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "no_warnings": True,
+            }
+            x = YoutubeDL(ydl_optssx)
+            info = x.extract_info(link, False)
+            xyz = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+            if os.path.exists(xyz):
+                return xyz
+            x.download([link])
+            return xyz
+
 
 
 async def bash(cmd):
