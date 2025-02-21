5c6f8d4657796a04ca94e0540ff3aa6153e3e919
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-21 17:45:16 +0530

    Update Vplay.py

--- Player/Plugins/Sounds/Vplay.py
+++ Player/Plugins/Sounds/Vplay.py
@@ -28,21 +28,21 @@ RPREFIX = config.RPREFIX
 
 async def ytdl(link):
     proc = await asyncio.create_subprocess_exec(
-            "yt-dlp",
-            "--cookies",
-            cookiefile(),
-            "-g",
-            "-f",
-            "best[height<=?720][width<=?1280]",
-            f"{link}",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await proc.communicate()
-        if stdout:
-            return 1, stdout.decode().split("\n")[0]
-        else:
-            return 0, stderr.decode()
+        "yt-dlp",
+        "--cookies",
+        cookiefile(),
+        "-g",
+        "-f",
+        "best[height<=?720][width<=?1280]",
+        f"{link}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        return 1, stdout.decode().split("\n")[0]
+    else:
+        return 0, stderr.decode()
 
 
 async def bash(cmd):
