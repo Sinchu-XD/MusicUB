751d62dc94b1194bf18a1335426673164fbe017d
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-21 14:39:18 +0530

    Update Play.py

--- Player/Plugins/Sounds/Play.py
+++ Player/Plugins/Sounds/Play.py
@@ -24,23 +24,33 @@ PREFIX = config.PREFIX
 
 RPREFIX = config.RPREFIX
 
+def cookiefile():
+    cookie_dir = "cookies"
+    cookies_files = [f for f in os.listdir(cookie_dir) if f.endswith(".txt")]
+
+    cookie_file = os.path.join(cookie_dir, cookies_files[0])
+    return cookie_file
+
 
 async def get_youtube_stream(link):
     loops = asyncio.get_running_loop()
     def get_stream_url():
         ydl_optssx = {
-            "format": "best",
-            "outtmpl": "downloads/%(id)s.%(ext)s",
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "quiet": True,
-            "no_warnings": True,
-            "cookiefile": "cookies.txt",
-        }
-        x = yt_dlp.YoutubeDL(ydl_optssx)
-        info = x.extract_info(link, False)
-        xyz = info['url']
-        return xyz
+                "cookiefile": cookiefile(),
+                "format": "bestaudio/best",
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "no_warnings": True,
+            }
+            x = YoutubeDL(ydl_optssx)
+            info = x.extract_info(link, False)
+            xyz = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+            if os.path.exists(xyz):
+                return xyz
+            x.download([link])
+            return xyz
         
     downloaded_file = await loops.run_in_executor(
         None, get_stream_url
