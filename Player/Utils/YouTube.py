0e6f1b4201bd342ed5414c949112890edd5e8b3f
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-21 14:52:58 +0530

    Create YouTube.py

---
+++ Player/Utils/YouTube.py
@@ -0,0 +1,52 @@
+import os
+from yt_dlp import YoutubeDL
+import asyncio
+
+
+
+def cookiefile():
+    cookie_dir = "cookies"
+    cookies_files = [f for f in os.listdir(cookie_dir) if f.endswith(".txt")]
+
+    cookie_file = os.path.join(cookie_dir, cookies_files[0])
+    return cookie_file
+
+
+async def get_youtube_stream(link):
+    loops = asyncio.get_running_loop()
+    def get_stream_url():
+        ydl_optssx = {
+                "cookiefile": cookiefile(),
+                "format": "bestaudio/best",
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "no_warnings": True,
+            }
+            x = YoutubeDL(ydl_optssx)
+            info = x.extract_info(link, False)
+            xyz = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+            if os.path.exists(xyz):
+                return xyz
+            x.download([link])
+            return xyz
+        
+    downloaded_file = await loops.run_in_executor(
+        None, get_stream_url
+    )
+    return downloaded_file
+
+
+
+
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
