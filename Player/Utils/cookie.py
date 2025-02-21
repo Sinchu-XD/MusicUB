313987abd51e6c48e23df2053ea41bf27c40687c
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-21 14:15:42 +0530

    Create cookie.py

---
+++ Player/Utils/cookie.py
@@ -0,0 +1,42 @@
+"""
+Telegram @Itz_Your_4Bhi
+Copyright ©️ 2025
+"""
+
+
+import os
+import requests
+from typing import Optional
+
+from config import config
+from ..logging import LOGGER
+
+
+def save_file(
+    pastebin_url: str, file_path: str = "Player/cookie/cookies.txt"
+) -> Optional[str]:
+    try:
+        response = requests.get(pastebin_url)
+        response.raise_for_status()
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "w") as file:
+            file.write(response.text)
+        return file_path
+
+    except requests.exceptions.RequestException as e:
+        LOGGER(__name__).error(f"Error fetching from {pastebin_url}: {e}")
+        return None
+
+
+def save_cookies() -> None:
+    full_url: str = str(config.COOKIES)
+    paste_id: str = full_url.split("/")[-1]
+    pastebin_url: str = f"https://batbin.me/raw/{paste_id}"
+
+    file_path = save_file(pastebin_url)
+    if file_path and os.path.getsize(file_path) > 0:
+        LOGGER(__name__).info(f"Cookies saved successfully to {file_path}.")
+    else:
+        LOGGER(__name__).error("Failed to save cookies or the file is empty.")
