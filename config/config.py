f89c87fdf46d5f676b2f443043007e4b36272072
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-30 13:06:54 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,7 +13,7 @@ API_HASH: str = os.getenv("API_HASH", "be8ea36c220d5e879c91ad9731686642")
 
 SESSION_STRING: str = os.getenv("SESSION_STRING", "BQBuoD8AtOwxLV-QRgi1IWpNidBHfwrIGV_5Imu1W5c_gOrWNumbb_zoZpp3NDSadfXkdFUk_bIc_UGDGnj4vTXt3VtLpWW9_lnmdjarZ4UuR9IkomRyM-FkdCoCTbUBzw_6a4j1z_fjGiWxW98WZ6CF1EVy3AfHFRBqrcn6-z4B2R-FhgPeew_CYk8vmXEpjYu4JK9EEGF8k-abTqI0GuGJ5W6pWNqa7QmB6rWPODMDkH21_DAgo8tsTVQId01dGrWso24MihswteFdRAzhA4Xho1_OXxN7amFL0WYsXEqRFh5tpIhW4jZ8KIMteqgfkgDsRRYjYCppGIs5tW7xwrS9AnzPewAAAAHjq--iAA")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 7143123520))]
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 7862043458))]
 
 LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", --1002369363228))
 
