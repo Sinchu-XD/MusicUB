1b8c593a304aa1f991ac7d8c8ee104626c1e7f03
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-02 12:54:27 +0530

    Update Play.py

--- Player/Plugins/Sounds/Play.py
+++ Player/Plugins/Sounds/Play.py
@@ -133,7 +133,7 @@ async def _aPlay(_, message):
                 f"**ѕσηg ιѕ ρℓαуιηg ιη ν¢**\n\n**SongName**:- [{title[:19]}]({link})\n**Duration**:- {duration}\n**Requested By**:- {mention}\n\n**Respose Time**:- {total_time_taken}",
                 disable_web_page_preview=True,
             )
-        await delete_messages(message, m)
+        asyncio.create_task(delete_messages(message, m))
 
 
 
@@ -183,7 +183,7 @@ async def playforce(_, message):
         f"**𝑅𝑒𝑠𝑝𝑜𝑛𝑠𝑒 𝑇𝑖𝑚𝑒**: {total_time_taken}",
         disable_web_page_preview=True,
     )
-    await delete_messages(message, m)
+    asyncio.create_task(delete_messages(message, m))
     
 
 
@@ -216,5 +216,5 @@ async def _raPlay(_, message):
                 f"**ѕσηg ιѕ ρℓαуιηg ιη ν¢**\n\n**SongName**:- [{message.reply_to_message.audio.title[:19]}]({message.reply_to_message.link})\n**Duration**:- {message.reply_to_message.audio.duration}\n**Requested By**:- {mention}\n\n**Response Time**:- {total_time_taken}",
                     disable_web_page_preview=True,
             )
-        await delete_messages(message, m)
+        asyncio.create_task(delete_messages(message, m))
 
