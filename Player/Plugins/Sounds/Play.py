6d6b2c85916f49ea9dbe551f34c063d1c986584d
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-02 13:32:34 +0530

    Update Play.py

--- Player/Plugins/Sounds/Play.py
+++ Player/Plugins/Sounds/Play.py
@@ -49,61 +49,66 @@ async def ytdl(format: str, link: str):
 async def processReplyToMessage(message):
     msg = message.reply_to_message
     if msg.audio or msg.voice:
-        m = await message.reply_text("Rukja...Tera Audio Download kar raha hu...")
+        m = await message.reply_text("**𝓦𝓪𝓲𝓽 𝓑𝓪𝓫𝔂... 𝓓𝓸𝔀𝓷𝓵𝓸𝓪𝓭𝓲𝓷𝓰 𝓨𝓸𝓾𝓻 𝓢𝓸𝓷𝓰 ❤️**.")
         audio_original = await msg.download()
         input_filename = audio_original
         return input_filename, m
     return None
 
 
+
 async def playWithLinks(link):
     if "&" in link:
-        pass
-    if "?" in link:
         pass
 
     return 0
 
 
 @app.on_message((filters.command(PLAY_COMMAND, [PREFIX, RPREFIX])) & filters.group)
+
 async def _aPlay(_, message):
     start_time = time.time()
     chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    mention = f"[{user_name}](tg://user?id={user_id})"
     if (message.reply_to_message) is not None:
         if message.reply_to_message.audio or message.reply_to_message.voice:
             input_filename, m = await processReplyToMessage(message)
             if input_filename is None:
-                await message.reply_text(
-                    "Audio pe reply kon karega mai? ya phir song link kon dalega mai? 🤔"
+                return await message.reply_text(
+                    "**𝙶𝚒𝚟𝚎 𝙼𝚎 𝚂𝚘𝚗𝚐 𝙻𝚒𝚗𝚔 𝙾𝚛 𝚁𝚎𝚙𝚕𝚢 𝚃𝚘 𝚅𝚘𝚒𝚌𝚎 𝙽𝚘𝚝𝚎😒**"
                 )
-                return
-            await m.edit("Rukja...Tera Audio Play karne vala hu...")
+                
+            await m.edit("𝑊𝑎𝑖𝑡 𝑁𝑎 𝑌𝑟𝑟𝑟 😒..")
             Status, Text = await Userbot.playAudio(chat_id, input_filename)
             if Status == False:
                 await m.edit(Text)
             else:
+                audio = message.reply_to_message.audio or message.reply_to_message.voice
+                audio_title = message.reply_to_message.text or "Unknown"
                 if chat_id in QUEUE:
                     queue_num = add_to_queue(
                         chat_id,
-                        message.reply_to_message.audio.title[:19],
-                        message.reply_to_message.audio.duration,
-                        message.reply_to_message.audio.file_id,
+                        audio_title[:19],
+                        audio.duration,
+                        audio.file_id,
                         message.reply_to_message.link,
                     )
-                    await m.edit(
-                        f"# {queue_num}\n{message.reply_to_message.audio.title[:19]}\nTera gaana queue me daal diya hu"
+                    return await m.edit(
+                        f"# {queue_num}\n{audio_title[:19]}\n**ʏᴏᴜʀ ꜱᴏɴɢ ᴀᴅᴅᴇᴅ ɪɴ Qᴜᴇᴜᴇ\nᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ 😵‍💫**"
                     )
-                    return
+
                 finish_time = time.time()
                 total_time_taken = str(int(finish_time - start_time)) + "s"
                 await m.edit(
-                    f"Tera gaana play kar rha hu aaja vc\n\nSongName:- [{message.reply_to_message.audio.title[:19]}]({message.reply_to_message.link})\nDuration:- {message.reply_to_message.audio.duration}\nTime taken to play:- {total_time_taken}",
+                    f"**ѕσηg ιѕ ρℓαуιηg ιη ν¢**\n\n**SongName**:- [{audio_title[:19]}]({message.reply_to_message.link})\n**Duration**:- {audio.duration}\n**Requested By**:- {mention}\n\n**Response Time**:- {total_time_taken}",
                     disable_web_page_preview=True,
                 )
     elif (len(message.command)) < 2:
-        await message.reply_text("Song name kon dalega mai? 🤔")
+        await message.reply_text("**𝑊𝑎𝑖𝑡 𝙶𝚒𝚟𝚎 𝙼𝚎 𝚂𝚘𝚗𝚐 𝙻𝚒𝚗𝚔 𝙾𝚛 𝚁𝚎𝚙𝚕𝚢 𝚃𝚘 𝚅𝚘𝚒𝚌𝚎 𝙽𝚘𝚝𝚎**")
     else:
-        m = await message.reply_text("Rukja...Tera gaana dhund raha hu...")
+        m = await message.reply_text("**Wait Na Yrrr 😒**")
         query = message.text.split(maxsplit=1)[1]
         video_id = extract_video_id(query)
         try:
@@ -111,12 +116,12 @@ async def _aPlay(_, message):
                 video_id = query
             title, duration, link = searchYt(video_id)
             if (title, duration, link) == (None, None, None):
-                return await m.edit("No results found")
+                await m.edit("No results found")
         except Exception as e:
-            await message.reply_text(f"Error:- <code>{e}</code>")
-            return
+            return await message.reply_text(f"Error:- <code>{e}</code>")
+        await m.edit("**ᴡᴀɪᴛ ɴᴀ ʏʀʀʀ\n\nꜱᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ꜱᴏɴɢ 🌚❤️..**")
 
-        await m.edit("Rukja...Tera gaana download kar raha hu...")
+
         format = "bestaudio"
         resp, songlink = await ytdl(format, link)
         if resp == 0:
@@ -124,23 +129,27 @@ async def _aPlay(_, message):
         else:
             if chat_id in QUEUE:
                 queue_num = add_to_queue(chat_id, title[:19], duration, songlink, link)
+
+
+
                 await m.edit(
-                    f"# {queue_num}\n{title[:19]}\nTera gaana queue me daal diya hu"
+                    f"# {queue_num}\n{title[:19]}\n**ʏᴏᴜʀ ꜱᴏɴɢ ᴀᴅᴅᴇᴅ ɪɴ Qᴜᴇᴜᴇ\n\nᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ 😵‍💫**"
                 )
-                return
-            # await asyncio.sleep(1)
+                
+
             Status, Text = await Userbot.playAudio(chat_id, songlink)
             if Status == False:
-                await m.edit(Text)
+                return await m.edit(Text)
             if duration is None:
-                duration = "Playing From LiveStream"
+                duration = "**Playing From LiveStream**"
             add_to_queue(chat_id, title[:19], duration, songlink, link)
             finish_time = time.time()
             total_time_taken = str(int(finish_time - start_time)) + "s"
             await m.edit(
-                f"Tera gaana play kar rha hu aaja vc\n\nSongName:- [{title[:19]}]({link})\nDuration:- {duration}\nTime taken to play:- {total_time_taken}",
+                f"**ѕσηg ιѕ ρℓαуιηg ιη ν¢**\n\n**SongName**:- [{title[:19]}]({link})\n**Duration**:- {duration}\n**Requested By**:- {mention}\n\n**Respose Time**:- {total_time_taken}",
                 disable_web_page_preview=True,
             )
+        asyncio.create_task(delete_messages(message, m))
 
 
 
