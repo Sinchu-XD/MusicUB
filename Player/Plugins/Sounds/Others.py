a6b6cb7bd955bfe463e0e91b7bd1de9ebc6eb5cb
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-02 22:11:02 +0530

    Auto Delete Command And Response

--- Player/Plugins/Sounds/Others.py
+++ Player/Plugins/Sounds/Others.py
@@ -3,240 +3,116 @@ Telegram @Itz_Your_4Bhi
 Copyright ©️ 2025
 """
 
-
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 import asyncio
 from Player import app
 from Player.Utils.Queue import clear_queue
 from Player.Utils.Loop import get_loop, set_loop
-from Player.Utils.Delete import delete_messages
 from Player.Core import Userbot
 from Player.Misc import SUDOERS
+from Player.Utils.Delete import delete_messages
 import config
 
-
 PREFIX = config.PREFIX
-
 RPREFIX = config.RPREFIX
 
-STOP_COMMAND = ["END", "CHUP"]
-
+STOP_COMMAND = ["STOP", "CHUP"]
 PAUSE_COMMAND = ["PAUSE"]
-
 RESUME_COMMAND = ["RESUME"]
-
 MUTE_COMMAND = ["MUTE"]
-
 UNMUTE_COMMAND = ["UNMUTE"]
-
 VOLUME_COMMAND = ["VOL", "VOLUME"]
-
 LOOP_COMMAND = ["LOOP"]
-
 LOOPEND_COMMAND = ["ENDLOOP"]
-
 
 @app.on_message(filters.command(STOP_COMMAND, PREFIX))
 async def _stop(_, message):
-    # Get administrators
-    administrators = []
-    async for m in app.get_chat_members(
-        message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-    ):
-        administrators.append(m)
-    if (message.from_user.id) in SUDOERS or (message.from_user.id) in [
-        admin.user.id for admin in administrators
-    ]:
-        m = await Userbot.stop(message.chat.id)
-        try:
-            clear_queue(message.chat.id)
-        except:
-            pass
-        await message.reply_text(m)
+    administrators = [
+        admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
+    ]
+    if message.from_user.id in SUDOERS or message.from_user.id in administrators:
+        Text = await Userbot.stop(message.chat.id)
+        clear_queue(message.chat.id)
+        m = await message.reply_text(Text)
     else:
-        return await message.reply_text(
-            "Abe saale... (Maaf karna wo gusse me thora sa idhar udhar ho jata hu) terepe perms naa hai admins ko bol..."
-        )
-        asyncio.create_task(delete_messages(message, m))
-
-
-@app.on_message(filters.command(STOP_COMMAND, RPREFIX))
-async def _stop(_, message):
-    if (len(message.command)) != 2:
-        await message.reply_text("You forgot to pass an argument")
-    else:
-        msg_id = message.text.split(" ", 1)[1]
-        m = await Userbot.stop(msg_id)
-        await message.reply_text(m)
-        asyncio.create_task(delete_messages(message, m))
-
+        m = await message.reply_text("Abe saale... terepe perms naa hai admins ko bol...")
+    asyncio.create_task(delete_messages(message, m))
 
 @app.on_message(filters.command(PAUSE_COMMAND, PREFIX))
 async def _pause(_, message):
-    # Get administrators
-    administrators = []
-    async for m in app.get_chat_members(
-        message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-    ):
-        administrators.append(m)
-    if (message.from_user.id) in SUDOERS or (message.from_user.id) in [
-        admin.user.id for admin in administrators
-    ]:
-        m = await Userbot.pause(message.chat.id)
-        await message.reply_text(m)
+    administrators = [
+        admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
+    ]
+    if message.from_user.id in SUDOERS or message.from_user.id in administrators:
+        Text = await Userbot.pause(message.chat.id)
+        m = await message.reply_text(Text)
     else:
-        return await message.reply_text(
-            "Abe saale... (Maaf karna wo gusse me thora sa idhar udhar ho jata hu) terepe perms naa hai admins ko bol..."
-        )
-        asyncio.create_task(delete_messages(message, m))
-
-
-@app.on_message(filters.command(PAUSE_COMMAND, RPREFIX))
-async def _pause(_, message):
-    if (len(message.command)) != 2:
-        await message.reply_text("You forgot to pass an argument")
-    else:
-        msg_id = message.text.split(" ", 1)[1]
-        m = await Userbot.pause(msg_id)
-        await message.reply_text(m)
-        asyncio.create_task(delete_messages(message, m))
-
+        m = await message.reply_text("Abe saale... terepe perms naa hai admins ko bol...")
+    asyncio.create_task(delete_messages(message, m))
 
 @app.on_message(filters.command(RESUME_COMMAND, PREFIX))
 async def _resume(_, message):
-    # Get administrators
-    administrators = []
-    async for m in app.get_chat_members(
-        message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-    ):
-        administrators.append(m)
-    if (message.from_user.id) in SUDOERS or (message.from_user.id) in [
-        admin.user.id for admin in administrators
-    ]:
-        m = await Userbot.resume(message.chat.id)
-        await message.reply_text(m)
+    administrators = [
+        admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
+    ]
+    if message.from_user.id in SUDOERS or message.from_user.id in administrators:
+        Text = await Userbot.resume(message.chat.id)
+        m = await message.reply_text(Text)
     else:
-        return await message.reply_text(
-            "Abe saale... (Maaf karna wo gusse me thora sa idhar udhar ho jata hu) terepe perms naa hai admins ko bol..."
-        )
-        asyncio.create_task(delete_messages(messag, m))
-
-
-@app.on_message(filters.command(RESUME_COMMAND, RPREFIX))
-async def _resume(_, message):
-    if (len(message.command)) != 2:
-        await message.reply_text("You forgot to pass an argument")
-    else:
-        msg_id = message.text.split(" ", 1)[1]
-        m = await Userbot.resume(msg_id)
-        await message.reply_text(m)
-        asyncio.create_task(delete_messages(message, m))
-
+        m = await message.reply_text("Abe saale... terepe perms naa hai admins ko bol...")
+    asyncio.create_task(delete_messages(message, m))
 
 @app.on_message(filters.command(MUTE_COMMAND, PREFIX))
 async def _mute(_, message):
-    if message.from_user and message.from_user.is_self:
-        reply = message.edit
-    else:
-        reply = message.reply_text
-    m = await Userbot.mute(message.chat.id)
-    await reply(m)
+    Text = await Userbot.mute(message.chat.id)
+    m = await message.reply_text(Text)
     asyncio.create_task(delete_messages(message, m))
-
-
-@app.on_message(filters.command(MUTE_COMMAND, RPREFIX))
-async def _mute(_, message):
-    if (len(message.command)) != 2:
-        await message.reply_text("You forgot to pass an argument")
-    else:
-        msg_id = message.text.split(" ", 1)[1]
-        m = await Userbot.mute(msg_id)
-        await message.reply_text(m)
-        asyncio.create_task(delete_messages(message, m))
-
 
 @app.on_message(filters.command(UNMUTE_COMMAND, PREFIX))
 async def _unmute(_, message):
-    m = await Userbot.unmute(message.chat.id)
-    await message.reply_text(m)
+    Text = await Userbot.unmute(message.chat.id)
+    m = await message.reply_text(Text)
     asyncio.create_task(delete_messages(message, m))
-
-
-@app.on_message(filters.command(UNMUTE_COMMAND, RPREFIX))
-async def _unmute(_, message):
-    if (len(message.command)) != 2:
-        await message.reply_text("You forgot to pass an argument")
-    else:
-        msg_id = message.text.split(" ", 1)[1]
-        m = await Userbot.unmute(msg_id)
-        await message.reply_text(m)
-        asyncio.create_task(delete_messages(message, m))
-
 
 @app.on_message(filters.command(VOLUME_COMMAND, PREFIX))
 async def _volume(_, message):
     try:
         vol = int(message.text.split()[1])
-        m = await Userbot.changeVolume(message.chat.id, vol)
+        Text = await Userbot.changeVolume(message.chat.id, vol)
     except:
-        m = await Userbot.changeVolume(message.chat.id)
-    await message.reply_text(m)
+        Text = await Userbot.changeVolume(message.chat.id)
+    m = await message.reply_text(Text)
     asyncio.create_task(delete_messages(message, m))
-
 
 @app.on_message(filters.command(LOOP_COMMAND, PREFIX))
 async def _loop(_, message):
-    # Get administrators
-    administrators = []
-    async for m in app.get_chat_members(
-        message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-    ):
-        administrators.append(m)
-    if (message.from_user.id) in SUDOERS or (message.from_user.id) in [
-        admin.user.id for admin in administrators
-    ]:
+    administrators = [
+        admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
+    ]
+    if message.from_user.id in SUDOERS or message.from_user.id in administrators:
         loop = await get_loop(message.chat.id)
         if loop == 0:
-            try:
-                await set_loop(message.chat.id, 1)
-                await message.reply_text(
-                    "Loop enabled. Now current song will be played 1 times"
-                )
-            except Exception as e:
-                return await message.reply_text(f"Error:- <code>{e}</code>")
-
+            await set_loop(message.chat.id, 5)
+            m = await message.reply_text("Loop enabled. Now current song will be played 5 times")
         else:
-            await message.reply_text("Loop already enabled")
+            m = await message.reply_text("Loop already enabled")
     else:
-        return await message.reply_text(
-            "Abe saale... (Maaf karna wo gusse me thora sa idhar udhar ho jata hu) terepe perms naa hai admins ko bol..."
-        )
-        asyncio.create_task(delete_messages(message))
-
+        m = await message.reply_text("Abe saale... terepe perms naa hai admins ko bol...")
+    asyncio.create_task(delete_messages(message, m))
 
 @app.on_message(filters.command(LOOPEND_COMMAND, PREFIX))
 async def _endLoop(_, message):
-    # Get administrators
-    administrators = []
-    async for m in app.get_chat_members(
-        message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-    ):
-        administrators.append(m)
-    if (message.from_user.id) in SUDOERS or (message.from_user.id) in [
-        admin.user.id for admin in administrators
-    ]:
+    administrators = [
+        admin.user.id async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
+    ]
+    if message.from_user.id in SUDOERS or message.from_user.id in administrators:
         loop = await get_loop(message.chat.id)
         if loop == 0:
-            await message.reply_text("Lopp is not enabled")
+            m = await message.reply_text("Loop is not enabled")
         else:
-            try:
-                await set_loop(message.chat.id, 0)
-                await message.reply_text("Loop Disabled")
-            except Exception as e:
-                return await message.reply_text(f"Error:- <code>{e}</code>")
+            await set_loop(message.chat.id, 0)
+            m = await message.reply_text("Loop Disabled")
     else:
-        return await message.reply_text(
-            "Abe saale... (Maaf karna wo gusse me thora sa idhar udhar ho jata hu) terepe perms naa hai admins ko bol..."
-  )
-        asyncio.create_task(delete_messages(message))
+        m = await message.reply_text("Abe saale... terepe perms naa hai admins ko bol...")
+    asyncio.create_task(delete_messages(message, m))
