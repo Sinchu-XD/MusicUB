9e57cca244bf9ddb53d0c5a8c5bffe930447d2d8
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-02 13:14:27 +0530

    Update Play.py

--- Player/Plugins/Sounds/Play.py
+++ Player/Plugins/Sounds/Play.py
@@ -130,9 +130,9 @@ async def _aPlay(_, message):
             await m.edit(f"❌ yt-dl issues detected\n\n» `{songlink}`")
         else:
             if chat_id in QUEUE:
-            queue_num = add_to_queue(chat_id, title[:19], duration, songlink, link)
-        else:
-            queue_num = None
+                queue_num = add_to_queue(chat_id, title[:19], duration, songlink, link)
+            else:
+                queue_num = None
 
                 await m.edit(
                     f"# {queue_num}\n{title[:19]}\n**ʏᴏᴜʀ ꜱᴏɴɢ ᴀᴅᴅᴇᴅ ɪɴ Qᴜᴇᴜᴇ\n\nᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ 😵‍💫**"
