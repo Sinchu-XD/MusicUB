90a94f6ea30cd76e73768c80719b8e7e776c69e7
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-09 14:20:10 +0530

    Update Delete.py

--- Player/Plugins/Sounds/Delete.py
+++ Player/Plugins/Sounds/Delete.py
@@ -1,6 +1,6 @@
 from Player import app
 from pyrogram import Client, filters
-
+from pyrogram.types import Message
 
 @app.on_message(filters.command("purgeall") & filters.me)
 async def purge_all_messages(client: Client, message: Message):
