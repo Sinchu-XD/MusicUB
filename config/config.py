91f8a848f8a548a129078b61bb171b8f36f66e22
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-23 13:23:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ import os
 
 API_ID: int = int(os.getenv("API_ID", 6067591))
 
-API_HASH: str = os.getenv("API_HASH", "94e17044c2393f43fda31d3afe77b26b)
+API_HASH: str = os.getenv("API_HASH", "94e17044c2393f43fda31d3afe77b26b")
 
 SESSION_STRING: str = os.getenv("SESSION_STRING", "BQBclYcAfwPhsOaYEN9rZiJTeqV1e-mW90J3pxU5lU-HRDBDir4n236Uy6xowZLnSJ83DDyV-7m8NommEpFKXVZMwRR41bXxvE8JzhIcLIJnCP5yObgE3yRkljsE36qEsdVYTgggdMSHrhoFWZG5YuOIJ0hi1HpqzOJhocARqoVbys1-CNSjTAEXdNB3knhatAqkHVnHfWcgvtshc3iiru3Gjpl9lXaPnLL5p5GP11dL8vRS4Dob-8nZW2vEkXqsD4-Ce6BAD8m4RIqTsomtrQCgaH4ugYfpFuKVr_oz04hUTjB4MzXK-Wr_Fz5Lk42PnrE3wWEwhsfgOVu8AM02YlKLV77MegAAAAHKUdR6AA")
 
