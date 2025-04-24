fc011c27aefcb3d02d323283a88e890d4a93879a
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-24 13:05:32 +0530

    Update Queue.py

--- Player/Utils/Queue.py
+++ Player/Utils/Queue.py
@@ -6,36 +6,23 @@ Copyright ©️ 2025
 
 QUEUE = {}
 
-
 def add_to_queue(chat_id, search_results, songlink, stream_url):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        chat_queue.append([chat_id, search_results, songlink, stream_url])
-        return int(len(chat_queue) - 1)
-    else:
-        QUEUE[chat_id] = [[chat_id, search_results, songlink, stream_url]]
-
+    data = [chat_id, search_results, songlink, stream_url]
+    if chat_id not in QUEUE:
+        QUEUE[chat_id] = []
+    QUEUE[chat_id].append(data)
+    return len(QUEUE[chat_id]) - 1
 
 def get_queue(chat_id):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        return chat_queue
-    else:
-        return 0
-
+    return QUEUE.get(chat_id, [])
 
 def pop_an_item(chat_id):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        chat_queue.pop(0)
-        return 1
-    else:
-        return 0
-
+    if chat_id in QUEUE and QUEUE[chat_id]:
+        QUEUE[chat_id].pop(0)
+        if not QUEUE[chat_id]:
+            QUEUE.pop(chat_id)
+        return True
+    return False
 
 def clear_queue(chat_id):
-    if chat_id in QUEUE:
-        QUEUE.pop(chat_id)
-        return 1
-    else:
-        return 0
+    return QUEUE.pop(chat_id, None) is not None
