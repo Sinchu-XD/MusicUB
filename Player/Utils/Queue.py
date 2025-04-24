458d044f6539617ab1bc27c27e8089d80136adfd
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-24 13:07:58 +0530

    Update Queue.py

--- Player/Utils/Queue.py
+++ Player/Utils/Queue.py
@@ -3,26 +3,32 @@ Telegram @Itz_Your_4Bhi
 Copyright ©️ 2025
 """
 
-
 QUEUE = {}
 
+
 def add_to_queue(chat_id, search_results, songlink, stream_url):
-    data = [chat_id, search_results, songlink, stream_url]
+    data = [search_results, songlink, stream_url]
     if chat_id not in QUEUE:
         QUEUE[chat_id] = []
     QUEUE[chat_id].append(data)
     return len(QUEUE[chat_id]) - 1
 
+
 def get_queue(chat_id):
     return QUEUE.get(chat_id, [])
+
 
 def pop_an_item(chat_id):
     if chat_id in QUEUE and QUEUE[chat_id]:
         QUEUE[chat_id].pop(0)
         if not QUEUE[chat_id]:
-            QUEUE.pop(chat_id)
+            del QUEUE[chat_id]
         return True
     return False
 
+
 def clear_queue(chat_id):
-    return QUEUE.pop(chat_id, None) is not None
+    if chat_id in QUEUE:
+        del QUEUE[chat_id]
+        return True
+    return False
