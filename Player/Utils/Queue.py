e5612ed3c97d672c305c11330b702ca657543270
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-27 14:18:05 +0530

    Update Queue.py

--- Player/Utils/Queue.py
+++ Player/Utils/Queue.py
@@ -8,43 +8,33 @@ QUEUE = {}
 import logging
 logging.basicConfig(level=logging.INFO)
 
-
 def add_to_queue(chat_id, search_results, songlink, stream_url):
     if chat_id in QUEUE:
         chat_queue = QUEUE[chat_id]
         chat_queue.append([chat_id, search_results, songlink, stream_url])
-        return int(len(chat_queue) - 1)
+        return len(chat_queue) - 1
     else:
         QUEUE[chat_id] = [[chat_id, search_results, songlink, stream_url]]
-
+        return 0 
 
 def get_queue(chat_id):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        return chat_queue
-    else:
-        return 0
-
+    return QUEUE.get(chat_id, [])
 
 def pop_an_item(chat_id):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        chat_queue.pop(0)
+    if chat_id in QUEUE and QUEUE[chat_id]:
+        QUEUE[chat_id].pop(0)
         return 1
-    else:
-        return 0
-
+    return 0
 
 def clear_queue(chat_id):
     if chat_id in QUEUE:
         QUEUE.pop(chat_id)
         return 1
-    else:
-        return 0
+    return 0
 
 def process_next_song(chat_id):
     queue_data = get_queue(chat_id)
-    if len(queue_data) > 0:
+    if queue_data:
         next_song_data = queue_data[0]
         
         if len(next_song_data) == 4:
@@ -55,4 +45,3 @@ def process_next_song(chat_id):
             logging.error(f"Invalid data in queue for chat_id {chat_id}: {next_song_data}")
     else:
         print(f"Queue is empty for chat_id: {chat_id}")
-        
