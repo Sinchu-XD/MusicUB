1794b734bbf7b7ddc9dfc97d8b7d81945543a9b6
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-04 13:26:38 +0530

    Create Tests.py

---
+++ Tests.py
@@ -0,0 +1,18 @@
+import asyncio
+from Player import call
+from Player.UserBot import playAudio
+from pytgcalls.types import MediaStream, VideoQuality, AudioQuality
+
+chat_id = "-1002523755325"
+audio_file = "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+
+# Functions
+async def play_audio():
+    try:
+        result, error = await playAudio(chat_id, audio_file)
+        if result:
+            print("Audio playing successfully!")
+        else:
+            print(f"Failed to play audio: {error}")
+    except Exception as e:
+        print(f"Error during audio playback: {e}")
